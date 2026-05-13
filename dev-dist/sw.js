30b9150df2d9ab937b695896c456902b12224130
Vikas Damre <vikasdamre@gmail.com>
2026-05-13 08:42:45 +0530

    correction in valid number

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-f87553f6'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.la0nu8ks5f"
+    "revision": "0.074vjm9vuv"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
